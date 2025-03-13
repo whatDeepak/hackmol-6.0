5a43cdbfe6668ef47d18f1b5cbea8fde8727dd5c
DHRUVKANDPAL <dhruvkandpal172@gmail.com>
2025-03-13 21:48:21 +0530

    added required svgs

---
+++ components/svg/Star.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+const Star = () => {
+  return (
+    <svg
+      width="68"
+      height="68"
+      viewBox="0 0 68 68"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M34.2765 0C41.3484 23.1205 50.03 28.4608 67.8789 34.2765C47.363 40.1262 39.2223 46.8344 33.6024 67.8789C29.194 45.9154 21.5246 38.7281 9.53674e-06 33.6024C19.573 27.5217 26.1702 19.1293 34.2765 0Z"
+        fill="url(#paint0_linear_871_1626)"
+      />
+      <defs>
+        <linearGradient
+          id="paint0_linear_871_1626"
+          x1="17.1382"
+          y1="16.8012"
+          x2="50.7407"
+          y2="51.0777"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop offset="0.315" stop-color="#94BBFF" />
+          <stop offset="0.7" stop-color="#D1E2FF" />
+          <stop offset="1" stop-color="#EAF1FF" />
+        </linearGradient>
+      </defs>
+    </svg>
+  );
+}
+
+export default Star
